df1577d74c6f0f68e4791c884d404b834ccc5d14
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-12-28 06:41:18 -0600

    fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
-import 'tailwindcss/tailwind.css'
-const hola = 'hola'
+import 'tailwindcss/tailwind.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
